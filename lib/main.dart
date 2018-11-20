6722b72e38ec7e6ca13616173955717c5c902e31
Jorge Becerra Rios <j.brios@udc.es>
2018-11-20 17:58:59 +0100

    scoped model

--- lib/main.dart
+++ lib/main.dart
@@ -2,143 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 import 'dart:io';
-
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-//import "package:path/path.dart" show dirname;
+import "package:path/path.dart" show dirname;
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:advanced_share/advanced_share.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'package:ipm_p2/model.dart';
+
 
 
 //void main() => runApp(new MyHomePage());
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp(
+    model: ImageModel(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final ImageModel model;
+
+  const MyApp({Key key, @required this.model}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'IPM-P2',
-      home:  MyHomePage(),
+    // At the top level of our app, we'll, create a ScopedModel Widget. This
+    // will provide the CounterModel to all children in the app that request it
+    // using a ScopedModelDescendant.
+    return ScopedModel<ImageModel>(
+      model: model,
+      child: MaterialApp(
+        title: 'Scoped Model Demo',
+        home: View(),////////////////////////
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
 
 
-class _MyHomePageState extends State<MyHomePage> {
-  File _image;
 
-  Future getImage() async {
-    //represent the results of asynchronous operations
-    var image = await ImagePicker.pickImage(source: ImageSource
-        .camera); //To suspend execution until a future completes
-
-    setState(() {
-      _image = image;
-    });
-  }
+class View  extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Image Picker Example'),
+        title: new Text('Cartoongram'),
       ),
-      body: new Center(
-        child: _image == null
-            ? new Text('No image selected.')
-        //: new Image.file(_image),
-            : new Image.file(convert_image()),
+      body:ScopedModelDescendant<ImageModel>(
+          builder: (context, child, model) {
+             return Center(
+              child: model.cartoon == null
+                  ? new Text('No se ha realizado ninguna fotografía')
+                  : new Image.file(model.cartoon),
+            );
+
+          },
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: new Icon(Icons.add_a_photo),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          ScopedModelDescendant<ImageModel>(
+          builder: (context, child, model) {
+          return  FloatingActionButton(
+            onPressed: model.getImage,
+            tooltip: 'Pick Image',
+            child: new Icon(Icons.camera_alt),
+            );
+          },
+
+          ),
+
+
+        ],
       ),
     );
   }
 
-  convert_image() async {
-    var image = File(_image.path);
-    var imageAsBytes = await image.readAsBytes();
-
-// Creating request
-// NOTE: In the emulator, localhost ip is 10.0.2.2
-    var uri = Uri.parse('http://127.0.0.1:5000/cartoon');
-    var request = http.MultipartRequest("POST", uri);
-    var inputFile = http.MultipartFile.fromBytes(
-        'image', imageAsBytes, filename: 'image.jpg');
-    request.files.add(inputFile);
-
-    //try {///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Sending request and waiting for response
-    var response = await request.send();
-    if (response.statusCode == 200) {
-// Receiving response stream
-      var responseStr = await response.stream.bytesToString();
-
-// Converting response string to json dictionary
-      var data = jsonDecode(responseStr);
-
-// Accessing response data
-      var cartoon = data['cartoon'];
-      if (cartoon != null) {
-// Creating the output file
-
-// Decoding base64 string received as response
-        var imageResponse = base64.decode(
-            cartoon);
-        return imageResponse;
-      }
-    }
-  }
-
-/***
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    class convert_image extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-    var image = ;/////////////////////////////////////////////////////////////////////////////////////////
-    var imageAsBytes = await image.readAsBytes();/////////////////////////////////////////////////////////////////////////
-
-    // Creating request
-    // NOTE: In the emulator, localhost ip is 10.0.2.2
-    var uri = Uri.parse('http://127.0.0.1:5000/cartoon');///////////////////////////////////////////////////////////////////
-    var request = http.MultipartRequest("POST", uri);////////////////////////////////////////////////////////////////////////////
-    var inputFile = http.MultipartFile.fromBytes('image', imageAsBytes, filename: 'image.jpg');///////////////////////////////////////
-    request.files.add(inputFile);//////////////////////////////////////////////////////////////////////////////////////////////////
-
-    try {///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Sending request and waiting for response
-    var response = await request.send();/////////////////////////////////////////////////////////////////////////////////////////////
-    if (response.statusCode == 200) {////////////////////////////////////////////////////////////////////////////////////////////
-    // Receiving response stream
-    var responseStr = await response.stream.bytesToString();/////////////////////////////////////////////////////////////////
-
-    // Converting response string to json dictionary
-    var data = jsonDecode(responseStr);///////////////////////////////////////////////////////////////////////////////////////
-
-    // Accessing response data
-    var cartoon = data['cartoon'];///////////////////////////////////////////////////////////////////////////////////////
-    if (cartoon != null) {///////////////////////////////////////////////////////////////////////////////////////////////
-    // Creating the output file
-
-    // Decoding base64 string received as response
-    var imageResponse = base64.decode(cartoon);/////////////////////////////////////////////////////////////////
-    return imageResponse
-    // Writing the decoded image to the output file
-    //await outputFile.writeAsBytes(imageResponse);////////////////////////////////////////////////////////////////
-
-    //print( 'Server is down');////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    }
 
-    }*/
 
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ lib/model.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import "package:path/path.dart" show dirname;
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:advanced_share/advanced_share.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class ImageModel extends Model {
+  File _image;
+  File _cartoon;
+  var _temp;
+  var i = 0;
+  Future<File> _imageFile;
+
+  File get cartoon => _cartoon;
+
+  Future getImage() async {
+    //represent the results of asynchronous operations
+    var image = await ImagePicker.pickImage(source: ImageSource
+        .camera); //To suspend execution until a future completes
+
+      _image = image;
+      notifyListeners();
+    var cartoon = await convertImage();
+
+      _cartoon = cartoon;
+      print(_cartoon);
+    notifyListeners();
+  }
+
+  convertImage() async{
+
+    // Getting the absolute path to this script file
+    //var currentPath = dirname(Platform.script.path);
+    // Generating  absolute paths for the input/output images
+    var pathImage=_image.path;
+    Directory appDocDir;
+    appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    i = i+1;
+    var pathCartoon= appDocPath+"/cartoon"+i.toString()+".png";
+    // Reading image
+    var image = File(pathImage);
+    var imageAsBytes = await image.readAsBytes();
+
+    // Creating request
+    // NOTE: In the emulator, localhost ip is 10.0.2.2
+    var uri = Uri.parse('http://10.0.2.2:5000/cartoon');//en el emulador
+    //var uri = Uri.parse('http://192.168.1.35:5000/cartoon');//en el movil
+    var request = http.MultipartRequest("POST", uri);
+    var inputFile = http.MultipartFile.fromBytes('image', imageAsBytes, filename: 'image.jpg');
+    request.files.add(inputFile);
+
+    try {
+      // Sending request and waiting for response
+      var response = await request.send();
+      if (response.statusCode == 200) {
+
+        // Receiving response stream
+        var responseStr = await response.stream.bytesToString();
+
+        // Converting response string to json dictionary
+        var data = jsonDecode(responseStr);
+
+        // Accessing response data
+        var cartoon = data['cartoon'];
+        if (cartoon != null) {
+          // Creating the output file
+          var outputFile = File(pathCartoon);
+          // Decoding base64 string received as response
+          var imageResponse = base64.decode(cartoon);
+
+          // Writing the decoded image to the output file
+          var f = await outputFile.writeAsBytes(imageResponse);
+
+          _temp=cartoon.toString();
+
+          return f;
+        }
+      }
+    } catch  (e) {
+      print( 'Server is down');
+    }
+  }
+}
+
+
+
